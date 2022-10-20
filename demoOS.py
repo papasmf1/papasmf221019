3cf9f8827aba034b31f7949649d200d5f5198dce
papasmf1 <papasmf1@gmail.com>
2022-10-20 13:45:50 +0900

    각종 모듈 소개

--- demoOS.py
+++ demoOS.py
@@ -5,3 +5,12 @@ print( abspath("python.exe") )
 print( basename("c:\\work\\demo1.py") )
 print( exists("c:\\python39\\python.exe") )
 
+#운영체제 정보
+from os import * 
+print("운영체제:", name)
+#system("notepad.exe")
+
+#파일 리스트 
+import glob 
+print( glob.glob("c:\\work2\\*.py") )
+

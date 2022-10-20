18536b93329ea1503c662bb8ffaf8dd062f40deb
papasmf1 <papasmf1@gmail.com>
2022-10-20 10:06:33 +0900

    파일 연습 완료

--- demoFile2.py
+++ demoFile2.py
@@ -6,7 +6,22 @@ f.close()
 
 #파일 읽기 
 f = open("c:\\work2\\demo.txt", "rt", encoding="utf-8")
+#전체 문자열을 str로 리턴 
 print( f.read() )
+
+print( f.tell() )
+#리셋(다시 처음으로 이동)
+f.seek(0)
+result = f.readlines()
+print( result )
+for item in result:
+    #코드를 보정 
+    print(item, end="")
+
+print("---한줄씩 읽기---")
+f.seek(0)
+print( f.readline(), end="" )
+print( f.readline(), end="" )
 
 f.close() 
 

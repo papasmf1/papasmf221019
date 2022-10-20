342313c904c975a9e895aca6292eee22f3b7736c
papasmf1 <papasmf1@gmail.com>
2022-10-20 13:38:46 +0900

    파일과 폴더 목록

---
+++ demoOS.py
@@ -0,0 +1,7 @@
+# demoOS.py 
+from os.path import * 
+
+print( abspath("python.exe") )
+print( basename("c:\\work\\demo1.py") )
+print( exists("c:\\python39\\python.exe") )
+
--- demoRandom.py
+++ demoRandom.py
@@ -4,10 +4,20 @@ import random
 print( random.random() )
 print( random.random() )
 
+#리스트 컴프리헨션(리스트 내장)
 print( [random.randrange(20) for i in range(10)] )
 print( [random.randrange(20) for i in range(10)] )
 
 print("---샘플링---")
 print( random.sample(range(20), 10) )
 print( random.sample(range(20), 10) )
+
+import math 
+#올림연산(4를 리턴)
+print( math.ceil(3.14) )
+#내림연산(3을 리턴)
+print( math.floor(3.14) )
+#반올림
+print( round(1.674, 2) )
+
 

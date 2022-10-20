31d1390e49390a51ac5649476d1ff4525d329cf8
papasmf1 <papasmf1@gmail.com>
2022-10-20 16:11:34 +0900

    네이버 웹툰 페이징처리

---
+++ web3.py
@@ -0,0 +1,21 @@
+# web2.py
+#웹서버와 통신
+import urllib.request
+#웹크롤링
+from bs4 import BeautifulSoup
+
+#페이징 처리
+for i in range(1,6):
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print( url )
+    data = urllib.request.urlopen(url)
+    #검색이 용이한 객체
+    soup = BeautifulSoup(data, "html.parser")
+    #필터링: <td class="title">
+    cartoons = soup.find_all("td", class_="title")
+    #<td class="title">
+    # 	<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+    # </td>
+    for item in cartoons:
+        title = item.find("a").text 
+        print(title)

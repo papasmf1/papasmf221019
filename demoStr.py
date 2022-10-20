e39b6cc7830d843b476e459b3d6c6617e08573e5
papasmf1 <papasmf1@gmail.com>
2022-10-20 10:45:14 +0900

    문자열 처리 연습2

--- demoStr.py
+++ demoStr.py
@@ -9,3 +9,19 @@ print( strA.capitalize() )
 print( strA.upper() )
 print( strA.count("p") )
 print( strA.count("p", 7) )
+
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
+
+u = "<<< spam and ham >>>"
+result = u.strip("<> ")
+print( u )
+print( result )
+result = result.replace("spam", "spam egg")
+print( result )
+lst = result.split()
+print( lst )
+print("---다시 하나로 합치기---")
+print( ":)".join(lst) )
+

b9aa91ba8170601476fdce854c90230926c89be6
papasmf1 <papasmf1@gmail.com>
2022-10-20 16:00:01 +0900

    네이버 웹튼 크롤링 2

--- web2.py
+++ web2.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
 #검색이 용이한 객체
 soup = BeautifulSoup(data, "html.parser")
+#필터링: <td class="title">
 cartoons = soup.find_all("td", class_="title")
 print("갯수:{0}".format( len(cartoons) ))
 title = cartoons[0].find("a").text 
@@ -14,5 +15,9 @@ link = cartoons[0].find("a")["href"]
 print( title )
 print( link )
 #<td class="title">
-# 	<a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+# 	<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
 # </td>
+
+for item in cartoons:
+    title = item.fine("a").text 
+    print(title)
